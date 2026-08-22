8d8c58ab57a85e751a0308a41c6d7253d4c883ec
Aladino Zulmar Abadi <sfateam8@gmail.com>
2026-08-22 16:19:34 +0700

    tambahka modul

--- lib/modules/activity/activity_page.dart
+++ lib/modules/activity/activity_page.dart
@@ -26,7 +26,9 @@ class _ActivityPageState extends State<ActivityPage> {
 
   Future<void> _refresh() async {
     final future = _load();
-    setState(() => _future = future);
+    setState(() {
+      _future = future;
+    });
     await future;
   }
 
